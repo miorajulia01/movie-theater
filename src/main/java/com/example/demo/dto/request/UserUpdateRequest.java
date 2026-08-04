5142b2393474b4c5d3c237008d3839e54d40c768
nassaigael <hei.nassai@gmail.com>
2026-08-04 21:55:02 +0300

    feat(dto): add validation to UserUpdateRequest
    Add validation constraints to UserUpdateRequest fields
    to ensure data consistency and accuracy. This
    improves data reliability and prevents invalid data.

--- src/main/java/com/example/demo/dto/request/UserUpdateRequest.java
+++ src/main/java/com/example/demo/dto/request/UserUpdateRequest.java
@@ -1,9 +1,15 @@
 package com.example.demo.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,7 +17,16 @@ import lombok.Setter;
 @Setter
 public class UserUpdateRequest {
 
+	@NotBlank
 	private String firstName;
+
+	@NotBlank
 	private String lastName;
+
+	@NotNull @Past private LocalDate birthdate;
+
+
+	@NotBlank
+	@Pattern(regexp = "^[+]?[0-9 ()-]{6,20}$")
 	private String phone;
 }
