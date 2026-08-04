f945dbc938b208d424f4b38333a473c105655cd8
nassaigael <hei.nassai@gmail.com>
2026-08-04 21:38:33 +0300

    feat(dto): add user request and response data transfer objects
    Introduce UserUpdateRequest and UserResponse to handle user
    data exchange between layers, improving data encapsulation
    and separation of concerns, making code more maintainable.

---
+++ src/main/java/com/example/demo/dto/request/UserUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserUpdateRequest {
+
+	private String firstName;
+	private String lastName;
+	private String phone;
+}
