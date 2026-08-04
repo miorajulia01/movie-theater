bd1b9038f8e3481c4644805add4958ab99a43b73
nassaigael <hei.nassai@gmail.com>
2026-08-04 20:56:23 +0300

    feat(entity): add User entity with attributes
    Introduce User entity to store user data,
    including personal details and role. This
    enables user management and authentication
    functionality in the application. It
    provides a foundation for further
    development of user-related features.

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,4 +1,45 @@
 package com.example.demo.entity;
 
+import com.example.demo.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(nullable = false)
+	private String firstName;
+
+	@Column(nullable = false)
+	private String lastName;
+
+	@Column(nullable = false)
+	private LocalDate birthdate;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String phone;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+
 }
